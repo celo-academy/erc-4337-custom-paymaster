cd5f6b976b5000297b79033a5688e0950b4c11e7
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-07-04 19:17:42 +0530

    chore: replace NFT contract address with a new one

--- sdk-calls.ts
+++ sdk-calls.ts
@@ -2,13 +2,13 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 export const claimCeloToken = async (sdk: ThirdwebSDK) => {
     const contract = await sdk.getContract(
-        "0xf72Ab3666c7302F5713FAEd4479b78a44fEAB89B" // celo alfajores
+        "0xf72Ab3666c7302F5713FAEd4479b78a44fEAB89B" // NFT contract on Celo Alfajores
     );
     const tx = await contract.erc20.mintTo(
         "0xdA82D492a49d08cF732A47Acf34efb51BE351dd6",
         "10"
     );
-    console.log("claimed", tx.receipt.transactionHash);
+    console.log("Claimed NFT Transaction Hash: ", tx.receipt.transactionHash);
     const tokenBalance = await contract.erc20.balance();
-    console.log("token balance:", tokenBalance.displayValue);
+    console.log("Token Balance: ", tokenBalance.displayValue);
 };
