1048773a0ddac6ac99258dd5ea58714e00691452
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-07-04 19:19:50 +0530

    chore: token type comment fix

--- sdk-calls.ts
+++ sdk-calls.ts
@@ -2,7 +2,7 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 export const claimCeloToken = async (sdk: ThirdwebSDK) => {
     const contract = await sdk.getContract(
-        "0xf72Ab3666c7302F5713FAEd4479b78a44fEAB89B" // NFT contract on Celo Alfajores
+        "0xf72Ab3666c7302F5713FAEd4479b78a44fEAB89B" // ERC20 contract on Celo Alfajores
     );
     const tx = await contract.erc20.mintTo(
         "0xdA82D492a49d08cF732A47Acf34efb51BE351dd6",
